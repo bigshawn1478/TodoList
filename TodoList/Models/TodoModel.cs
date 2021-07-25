a94176ceefb0541b197406c7c3bdaca1ae63a4ac
bigshawn1478 <bigshawn1478@gmail.com>
2021-07-25 15:34:51 +0800

    新增TodoModel

---
+++ TodoList/Models/TodoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoList.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ModifyDate { get; set; }
+    }
+}
